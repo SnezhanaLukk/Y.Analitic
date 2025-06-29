7b0057246f1cd2815e256ebc6d56994413e4129a
SnezhanaLukk <lukkarevasa@gmail.com>
2025-06-29 23:41:22 +0300

    Доработана проверка генерации файла

--- src/tests/e2e/specs/downloadGenerateFile.spec.ts
+++ src/tests/e2e/specs/downloadGenerateFile.spec.ts
@@ -2,11 +2,14 @@ import { test, expect } from '@playwright/test';
 
 test('Проверка загрузки сгенерированного файла', async ({ page }) => {
     await page.goto('http://localhost:5173/generate');
-    await page.locator('button:has-text("Начать генерацию")').click();
-    const [download] = await Promise.all([page.waitForEvent('download')]);
+
+    const [download] = await Promise.all([
+        page.waitForEvent('download'),
+        page.getByRole('button', { name: 'Начать генерацию' }).click(),
+    ]);
 
     const suggestedFilename = download.suggestedFilename();
-    expect(suggestedFilename).toMatch(/.*\.csv$/);
+    expect(suggestedFilename).toMatch(/\.csv$/);
 
     const path = await download.path();
     expect(path).not.toBeNull();
